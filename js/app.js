9534c1146c5bb9ee81839daa97b15d3011059ef9
nedalalashqar <nedal.r.alashqar@gmail.com>
2021-04-26 17:02:02 +0300

    lab12

--- js/app.js
+++ js/app.js
@@ -34,26 +34,12 @@ const rightImage = document.getElementById( 'rightImage' );
 const viewResult = document.getElementById( 'viewResult' );
 const resultContainer = document.getElementById( 'res' );
 
-
-
-// let img = ['image 1', 'image 2', 'image 3', 'image 4', 'image 5', 'image 6', 'image 7', 'image 8', 'image 9', 'image 10' ,
-//'image 11', 'image 12' , 'image 13' , 'image 14' , 'image 15' , 'image 16' , 'image 17' , 'image 18' 'image 19' , 'image 20'
-//, 'image 21' , 'image 22'
-//]
-
 let clickNumber = 0;
 let leftImageIndex = 0;
 let rightImageIndex = 0;
 let mediumImageIndex = 0;
 let attempt=25;
 
-//let Results = {
-//  name: 'Results',
-//},
-
-//$.getScript('js/app.js' , function){
-    
-//}
 
 function Ima( name , img ) {
   this.name = name.split('.')[0];
@@ -128,7 +114,6 @@ function renderhart() {
     names.push( Ima.all[i].names );
     shown.push( Ima.all[i].shown );
   }
-
 
   let ctx = document.getElementById( 'myChart' ).getContext( '2d' );
   let myChart = new Chart( ctx, {
